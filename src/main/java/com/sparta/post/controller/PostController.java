e298f81bccc9fd5bc55ce9e3f44e0afa862fced6
kwangkyun kim <kyuni9786@naver.com>
2023-06-29 05:53:15 +0900

    컨트롤러

--- src/main/java/com/sparta/post/controller/PostController.java
+++ src/main/java/com/sparta/post/controller/PostController.java
@@ -1,11 +1,45 @@
 package com.sparta.post.controller;
 
+import com.sparta.post.dto.PostRequestDto;
+import com.sparta.post.dto.PostResponseDto;
+import com.sparta.post.service.PostService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class PostController {
+    private final PostService postService;
+
+    @GetMapping("/posts")
+    public List<PostResponseDto> getPosts() {
+        return postService.getPosts();
+    }
+
+    @GetMapping("/post/{id}")
+    public PostResponseDto getPostById(@PathVariable Long id) {
+        return postService.findPostResponse(id);
+    }
+
+    @PostMapping("/post")
+    public PostResponseDto createPost(@RequestBody PostRequestDto requestDto) {
+        return postService.createPost(requestDto);
+    }
+
+    @PutMapping("/post/{id}")
+    public PostResponseDto updatePost(@PathVariable Long id, @RequestBody PostRequestDto requestDto) {
+        return postService.updatePost(id, requestDto);
+    }
+
+    @DeleteMapping("/post/{id}")
+    public @ResponseBody Map<String, Boolean> deletePost(@PathVariable Long id, @RequestBody Map<String, String > password) {
+        Map<String, Boolean> deleteResponse = new HashMap<>();
+        deleteResponse.put("success", postService.deletePost(id, password.get("password")));
+        return deleteResponse;
+    }
 }
