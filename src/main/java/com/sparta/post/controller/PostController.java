d9945133a2db5e954803bd54d7f39c3763ee6a86
kwangkyun kim <kyuni9786@naver.com>
2023-06-29 20:39:06 +0900

    애노테이션 메소드 정리 및 마무리

--- src/main/java/com/sparta/post/controller/PostController.java
+++ src/main/java/com/sparta/post/controller/PostController.java
@@ -11,35 +11,52 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+// REST API 요청을 처리하는 컨트롤러 // JSON
 @RequestMapping("/api")
+// 기본경로 지정
 @RequiredArgsConstructor
+// 생성자 주입 ( PostService 객체를 생성자에서 주입 )
 public class PostController {
     private final PostService postService;
 
+    // 모든 게시물 조회 요청에 대한 핸들러
     @GetMapping("/posts")
+    //  "/posts" 경로로 GET 요청이 오면 이 메서드가 실행됩니다.
     public List<PostResponseDto> getPosts() {
         return postService.getPosts();
+        // postService 를 통해 모든 게시물을 조회한 결과를 반환합니다.
     }
 
+    // 특정 게시물 조회 요청에 대한 핸들러
     @GetMapping("/post/{id}")
     public PostResponseDto getPostById(@PathVariable Long id) {
         return postService.findPostResponse(id);
+        // getPostById(Long id): 특정 게시물을 조회
     }
 
+    // 게시물 생성 요청에 대한 핸들러
     @PostMapping("/post")
     public PostResponseDto createPost(@RequestBody PostRequestDto requestDto) {
         return postService.createPost(requestDto);
-    }
+    }  // requestDto 를 통해 게시물을 생성한 결과를 반환합니다.
 
+    // 게시물 수정 요청에 대한 핸들러
     @PutMapping("/post/{id}")
     public PostResponseDto updatePost(@PathVariable Long id, @RequestBody PostRequestDto requestDto) {
         return postService.updatePost(id, requestDto);
-    }
+    } // id에 해당하는 게시물을 requestDto의 내용으로 수정한 결과를 반환합니다.
 
+    // 게시물 삭제 요청에 대한 핸들러
     @DeleteMapping("/post/{id}")
-    public @ResponseBody Map<String, Boolean> deletePost(@PathVariable Long id, @RequestBody Map<String, String > password) {
+    public @ResponseBody Map<String, Boolean>
+    deletePost(@PathVariable Long id, @RequestBody Map<String, String> password) {
+       // id에 해당하는 게시물을 password 에 해당하는 비밀번호로 삭제한 결과를 반환합니다.
+        // 응답 결과를 JSON 형식으로 반환하도록 지정합니다. ( @RequestBody)
         Map<String, Boolean> deleteResponse = new HashMap<>();
+        //  응답 결과를 담을 맵 객체를 생성합니다.
         deleteResponse.put("success", postService.deletePost(id, password.get("password")));
+        // 삭제 결과를 deleteResponse 맵에 "success" 키로 저장합니다.
         return deleteResponse;
+
     }
 }
--- src/main/java/com/sparta/post/entity/Post.java
+++ src/main/java/com/sparta/post/entity/Post.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Column;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -17,16 +18,28 @@ public class Post extends Timestamped {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
+    // nullable = false: 해당 컬럼이 null 값을 허용하지 않음을 나타냅니다.
     private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    // columnDefinition = "TEXT": 해당 컬럼의 데이터 타입을 "TEXT"로 지정합니다.
+    // 이는 긴 텍스트 데이터를 저장할 수 있는 타입입니다. (기본값은 VARCHAR 입니다.)
     private String content;
+
+    @Column(nullable = false)
     private String author;
+
+    @Column(nullable = false)
     private String password;
-    public Post(String title, String content, String author, String password) {
-        this.title = title;
-        this.content = content;
-        this.author = author;
-        this.password = password;
-    }
+
+//    public Post(String title, String content, String author, String password) {
+//        this.title = title;
+//        this.content = content;
+//        this.author = author;
+//        this.password = password;
+//    }
 
     public Post(PostRequestDto requestDto) {
         this.title = requestDto.getTitle();
